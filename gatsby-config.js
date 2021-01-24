dd45332d576eebd6a016a77a42437f5599542e7c
dimitriosweb <dimitrios.web@outlook.com>
2021-01-24 17:03:01 +0100

    style(site): logo and global primary color

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,15 +44,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'xilinapatomata',
+        short_name: 'xilinapatomata',
         start_url: '/',
-        background_color: '#00C2BD',
-        theme_color: '#00C2BD',
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        background_color: '#6d8536',
+        theme_color: '#6d8536',
         display: 'standalone',
-        icon: `${__dirname}/static/images/logo.svg` // This path is relative to the root of the site.
+        icon: `${__dirname}/static/images/logo.png` // This path is relative to the root of the site.
       }
     },
 
