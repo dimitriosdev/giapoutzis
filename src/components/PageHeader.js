8415d26ab468a3035079a85fe78a9d3964d9a1cd
dimitriosweb <dimitrios.web@outlook.com>
2021-01-24 23:05:56 +0100

    feat(global): video hero

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Image from './Image'
+import BackgroundVideo from '../components/BackgroundVideo'
 import Content from './Content'
 import './PageHeader.css'
 
+
+const videoPoster = ''
+const videoTitle = ''
+const video = 'https://res.cloudinary.com/duttuhph7/video/upload/v1611525654/part1_her6gs.mp4'//'https://res.cloudinary.com/duttuhph7/video/upload/v1611521346/final_n3k2h2.mp4'
 const PageHeader = ({
   title,
   subtitle,
@@ -15,15 +19,10 @@ const PageHeader = ({
   if (large) className += ' PageHeader-large'
   return (
     <div className={`PageHeader relative ${className}`}>
-      {backgroundImage && (
-        <Image
-          background
-          resolutions="large"
-          src={backgroundImage}
-          alt={title}
-          size="cover"
-        />
-      )}
+
+      <BackgroundVideo poster={videoPoster} videoTitle={videoTitle}>
+        {video && <source src={video} type="video/mp4" />}
+      </BackgroundVideo>
       <div className="PageHeader__content container relative">
         <h1 className="PageHeader--Title">{title}</h1>
         {subtitle && (
