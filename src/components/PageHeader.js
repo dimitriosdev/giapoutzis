6ca98f58c87c6f2827a4cd2b77a30a0125533dc4
dimitriosweb <dimitrios.web@outlook.com>
2021-01-25 21:35:08 +0100

    merge

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -6,12 +6,6 @@ import Content from './Content'
 import BackgroundVideo from '../components/BackgroundVideo'
 import './PageHeader.css'
 
-<<<<<<< HEAD
-const video = 'https://res.cloudinary.com/duttuhph7/video/upload/v1611437278/wood_plyauq'
-const videoPoster = ''
-=======
-
->>>>>>> f012355f483022c47e850018e6b535e60911d30f
 const PageHeader = ({
   title,
   subtitle,
@@ -22,15 +16,6 @@ const PageHeader = ({
   if (large) className += ' PageHeader-large'
   return (
     <div className={`PageHeader relative ${className}`}>
-<<<<<<< HEAD
-      {backgroundImage && (
-        <section className="BackgroundVideo-section section">
-          <BackgroundVideo poster={videoPoster} videoTitle={title} videoSubtitle={subtitle}>
-            {video && <source src={video} type="video/mp4" />}
-          </BackgroundVideo>
-        </section>
-      )}
-=======
 
       <BackgroundVideo poster='' videoTitle=''>
         {heroVideo && <source src={heroVideo} type="video/mp4" />}
@@ -41,7 +26,6 @@ const PageHeader = ({
           <Content className="PageHeader--Subtitle" src={subtitle} />
         )}
       </div>
->>>>>>> f012355f483022c47e850018e6b535e60911d30f
     </div>
   )
 }
